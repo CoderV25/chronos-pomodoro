b3a109dc81acb0dde2e1fefea7a2c8c92816112a
Gabriel Vinicius <viniciusreboucas2501@gmail.com>
2026-01-09 16:56:13 -0300

    Add task cycle management functions and integrate with MainForm

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -5,10 +5,18 @@ import { Cycles } from "../Cycles";
 import { useRef } from "react";
 import type { TaskModel } from "../../models/TaskModel";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
+import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
 
 export function MainForm() {
-  const { setState } = useTaskContext();
+  const { state, setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
+
+  //ciclos
+
+  const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -28,8 +36,8 @@ export function MainForm() {
       startDate: Date.now(),
       completeDate: null,
       interruptDate: null,
-      duration: 1,
-      type: "workTime",
+      duration: state.config[nextCycleType],
+      type: nextCycleType,
     };
 
     const secondsRemaining = newTask.duration * 60;
@@ -39,9 +47,9 @@ export function MainForm() {
         ...prevState,
         config: { ...prevState.config },
         activeTask: newTask,
-        currentCycle: 1, //Conferir
+        currentCycle: nextCycle, //Conferir
         secondsRemaining, //Conferir
-        formattedSecondsRemaining: "00:00", //Conferir
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining), //Conferir
         tasks: [...prevState.tasks, newTask],
       };
     });
